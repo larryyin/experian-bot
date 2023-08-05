980e457a4e1e62da8e9bd2e1212dc25a7077858c
Larry Yin <larryyin@air23.local>
2023-08-05 06:40:05 -0700

    Removed nohup

--- deploy/run_bot.sh
+++ deploy/run_bot.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Run python script in the background and detach the session
-nohup /home/$(whoami)/gpt/bin/python bot.py >> output.log 2>&1 &
-
-# Disown the process to make sure it keeps running after the terminal is closed
-disown
+/home/$(whoami)/gpt/bin/python bot.py
